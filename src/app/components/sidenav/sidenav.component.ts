ee0b773ce01a6c7f9d99863d854c7d08abb963b0
YevgenChop <yevgenchop@gmail.com>
2022-11-21 15:07:48 +0100

    feat: add log out

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,8 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { AuthUserStore } from '../../store/auth-user.store';
+import { AuthService } from '../auth/services/auth.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,7 +10,16 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
+  @ViewChild('drawer') public drawer!: MatDrawer;
   public isMobile!: boolean;
+  public authUser$ = this.authUserStore.authUser$;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private authUserStore: AuthUserStore
+  ) {}
+
   ngOnInit() {
     this.isMobile = window.innerWidth < 768;
   }
@@ -14,5 +27,10 @@ export class SidenavComponent implements OnInit {
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.isMobile = window.innerWidth < 768;
+  }
+
+  public logout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('auth/login');
   }
 }
